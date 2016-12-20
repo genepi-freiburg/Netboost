85a5d803ba990a7e8a18ec1b8d6d928b0d823814
Jochen Knaus <jo@zeitlupe.org>
2016-12-20 12:58:04 +0100

    Parameter checks for disto_tom() function

--- R/netboost.R
+++ R/netboost.R
@@ -13,3 +13,23 @@ netboost <- function() {
   
   system(prg)
 }
+
+#' Calculate distance
+#' (external wrapper for internal C++ function)
+#' 
+#' @param filter Filter-Matrix
+#' @param adjacency Adjacency-Vector
+#' @return Vector with distances (same length as adjacency)
+#' @export
+dist_tom <- function(filter=NULL, adjacency=NULL) {
+  if (is.null(filter) || is.null(adjacency))
+    stop("Both filter and adjacency must be provided")
+
+  if (!(is.matrix(filter) && (nrow(filter) > 0) && (ncol(filter) > 0)))
+    stop("filter must be matrix with dim() > (0,0)")
+
+  if (!(is.vector(adjacency) && (length(adjacency) > 0)))
+    stop("adjacency is required a vector with length > 0")
+
+  return(netboost:::rcpp_dist_tom(filter, adjacency))
+}
