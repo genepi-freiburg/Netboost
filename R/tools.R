eff9ec9bf104481e4fbb4060684b4683122ccaa0
Jochen Knaus <jo@zeitlupe.org>
2019-04-23 14:18:59 +0200

    Removal internal and own temp

--- R/tools.R
+++ R/tools.R
@@ -11,57 +11,52 @@
 #'   mcupgma_exec(exec="cluster.pl", "--help")
 #' @export
 mcupgma_exec <- function(exec=NULL, ..., console=TRUE) {
-  if (is.null(exec)) stop("mcupgma_exec: call without executable")
-  
-  exec_abs_path <- file.path(netboostMCUPGMAPath(), exec)
-  
-  if (!file.exists(exec_abs_path))
-    stop(paste("mcupgma_exec: file does not exist:", exec, exec_abs_path))
+    if (is.null(exec)) stop("mcupgma_exec: call without executable")
 
-  # Types may become mixed giving many parameters, so ensure boolean.
-  console <- isTRUE(as.logical(console))
+    exec_abs_path <- file.path(netboostMCUPGMAPath(), exec)
 
-  # Arguments must be bundled in character vector
-  call_args <- as.character(unlist(list(...), use.names=FALSE))
-  
-  # Default: STDOUT/STDERR to R console
-  std <- ""
-  
-  # If console is not set, write output to temporary file.
-  if (!console) std <- tempfile()
-  
-  # Execute command (always fetch return code)
-#  print(paste("Starting:", exec_abs_path))
-#  print(paste(..., sep=",", collate=""))
-#  flush.console()
-  
-  ret <- system2(command = exec_abs_path,
-                 stdout = std, stderr = std,
-                 wait=TRUE,
-                 args=call_args,
-                 env=c(paste0("TMP_PATH=", netboostTmpPath())))
+    if (!file.exists(exec_abs_path))
+        stop(paste("mcupgma_exec: file does not exist:", exec, exec_abs_path))
 
-#  flush.console()
-  
-  # If execution returned error, always throw warning.
-  # Also, save copy of output.
-  if (ret != 0) {
-    preserve <- file.path(netboostTmpPath(), paste0("output_", exec))
-    file.copy(std, preserve)
-    warning(paste("Execution of", exec, "returned no success:", ret,
-                  "(this may be an real error or not).",
-                  "STDOUT/ERR output saved in:", preserve),
-            call. = FALSE)
-#    file.show(std)
-  }
-  
-  # If STDIN/STDERR was printed on console, return exit code.
-  # Else, read output from temporary file and return as text.
-  if (console) {
-    return(ret)
-  } else {
-    std_output <- readChar(std, file.info(std)$size)
-    unlink(std)
-    return(std_output)
-  }
+    # Types may become mixed giving many parameters, so ensure boolean.
+    console <- isTRUE(as.logical(console))
+
+    # Arguments must be bundled in character vector
+    call_args <- as.character(unlist(list(...), use.names=FALSE))
+    
+    # Default: STDOUT/STDERR to R console
+    std <- ""
+
+    # If console is not set, write output to temporary file.
+    if (!console) std <- tempfile()
+
+    ret <- system2(command = exec_abs_path,
+                   stdout = std, stderr = std,
+                   wait=TRUE,
+                   args=call_args,
+                   env=c(paste0("TMP_PATH=", netboostTmpPath())))
+
+    #  flush.console()
+
+    # If execution returned error, always throw warning.
+    # Also, save copy of output.
+    if (ret != 0) {
+        preserve <- file.path(netboostTmpPath(), paste0("output_", exec))
+        file.copy(std, preserve)
+        warning(paste("Execution of", exec, "returned no success:", ret,
+                      "(this may be an real error or not).",
+                      "STDOUT/ERR output saved in:", preserve),
+                call. = FALSE)
+        #    file.show(std)
+    }
+    
+    # If STDIN/STDERR was printed on console, return exit code.
+    # Else, read output from temporary file and return as text.
+    if (console) {
+        return(ret)
+    } else {
+        std_output <- readChar(std, file.info(std)$size)
+        unlink(std)
+        return(std_output)
+    }
 }
--- src/install.libs.R
+++ src/install.libs.R
@@ -9,57 +9,57 @@
 #'
 #' Oct 2017, jo
 
-# Internal helper: create visible error messages.
+#' Internal helper: create visible error messages.
 nbErrorMsg <- function(msg="") {
-  row <- paste0(rep("*", 70), collapse = "")
+    row <- paste0(rep("*", 70), collapse = "")
 
-  return(paste0(c("", row, msg, row, ""), collapse = "\n", sep = ""))
+    return(paste0(c("", row, msg, row, ""), collapse = "\n", sep = ""))
 }
 
-# Internal helper: check if a given program is installed
-# (simple check: calling creates an error)
+#' Internal helper: check if a given program is installed
+#' (simple check: calling creates an error)
 nbCheckPrg <- function(prg, txt) {
-  # Check if program is present (all feature --version or ignore it)
-  program <- tryCatch(system2(prg, "--version",
-                              stdout = TRUE, stderr = TRUE),
-                      error=function(err) NULL)
-  
-  if (is.null(program))
-    return(paste(txt, " not installed (call '", prg, "')", sep=""))
-  else
-    return(NULL)
+    # Check if program is present (all feature --version or ignore it)
+    program <- tryCatch(system2(prg, "--version",
+                                stdout = TRUE, stderr = TRUE),
+                        error=function(err) NULL)
+
+    if (is.null(program))
+        return(paste(txt, " not installed (call '", prg, "')", sep=""))
+    else
+        return(NULL)
 }
 
+# Should never be reached, as in DESCRIPTION and .BBSfile.
 if (WINDOWS)
-  stop(nbErrorMsg("mcupgma build on Windows is currently not supported"))
+    stop(nbErrorMsg("mcupgma build on Windows is currently not supported"))
 
 # Check if required prequisites for mcupgma are all available
 # (as in SystemRequirements in DESCRIPTION + others)
 cnt <- c(nbCheckPrg("make", "(GNU) make"),
          nbCheckPrg("bash", "Bash shell"),
          nbCheckPrg("perl", "Perl interpreter"),
-#         nbCheckPrg("gzip", "GZip packer"),
          nbCheckPrg("echo", "echo shell command"))
+#         nbCheckPrg("gzip", "GZip packer"),
 
 # Any missings?
 if (length(cnt) > 0) {
-  # List formating could be done more elegant...
-  cnt[1] <- paste0("- ", cnt[1])
-  stop(nbErrorMsg(gsub("\n$", "", perl = TRUE,
-                       paste0(cnt, sep = "\n", collapse = "- "))))
+    # List formating could be done more elegant...
+    cnt[1] <- paste0("- ", cnt[1])
+    stop(nbErrorMsg(gsub("\n$", "", perl = TRUE,
+                         paste0(cnt, sep = "\n", collapse = "- "))))
 }
 
-
-#print(paste("R_PACKAGE_NAME", R_PACKAGE_NAME))
-print(paste("R_PACKAGE_SOURCE", R_PACKAGE_SOURCE))
-print(paste("R_PACKAGE_DIR", R_PACKAGE_DIR))
-print(paste("R_ARCH", R_ARCH))
-print(paste("SHLIB_EXT", SHLIB_EXT))
+# Those variables are coming from R.
+print(paste("R_PACKAGE_SRC: ", R_PACKAGE_SOURCE))
+print(paste("R_PACKAGE_DIR: ", R_PACKAGE_DIR))
+print(paste("R_ARCH:        ", R_ARCH))
+print(paste("SHLIB_EXT:     ", SHLIB_EXT))
 
 ## mcupgma lying in src folder
 src_mcupgma  <- file.path(R_PACKAGE_SOURCE, 'src', 'mcupgma')
 
-# Destination path of R package
+## Destination path of R package
 dest_lib <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 
 ## MCUPGMA folder (binaries, scripts and additional files)
@@ -68,9 +68,9 @@ dest_mcupgma <- file.path(R_PACKAGE_DIR, 'mcupgma')
 print(paste("INSTALL PATH: ",
             file.path(src_mcupgma, "scripts", "install_path.mk")))
 
-# Original MCUPGMA Makefile: create installation path as separate file with
-# only export of variable INSTALL_PATH.
-# @TODO Replace with R file writing.
+## Original MCUPGMA Makefile: create installation path as separate file with
+## only export of variable INSTALL_PATH.
+## @TODO Replace with R file writing.
 system2("echo", paste0("export INSTALL_PATH := ",
                        dest_mcupgma,
                        " > ",
@@ -84,7 +84,7 @@ retMake1 <- system2(command = "make",                ## Clean
 
 ## Stop on error (default)
 if (retMake1 != 0)
-  stop(nbErrorMsg("Build mcupgma: make clean failed"))
+    stop(nbErrorMsg("Build mcupgma: make clean failed"))
 
 ## Build all.
 retMake2 <- system2(command = "make",
@@ -93,15 +93,14 @@ retMake2 <- system2(command = "make",
                              "all"))
 
 if (retMake2 != 0)
-  stop(nbErrorMsg("Build mcupgma: make all failed"))
+    stop(nbErrorMsg("Build mcupgma: make all failed"))
 
 ## TODO Add checks.
-
 print(paste("SRC: ", src_mcupgma))
 print(paste("DEST:", dest_mcupgma))
 
 if (!dir.create(dest_mcupgma, recursive = TRUE, showWarnings = TRUE))
-  stop(nbErrorMsg(paste("Unable to create:", dest_mcupgma)))
+    stop(nbErrorMsg(paste("Unable to create:", dest_mcupgma)))
 
 ## Executables created in two directories (clustering_round and
 ## clustering_utils) plus some Shell/Perl scripts in directory
@@ -111,65 +110,63 @@ if (!dir.create(dest_mcupgma, recursive = TRUE, showWarnings = TRUE))
 for (path in c(file.path('clustering_round', 'bin'),
                file.path('clustering_util', 'bin'),
                file.path('scripts'))) {
-  ## Source folder
-  copy_path <- file.path(src_mcupgma, path)
-  
-  ## All files in folder
-  files <- list.files(path = copy_path, full.names = TRUE)
+    ## Source folder
+    copy_path <- file.path(src_mcupgma, path)
 
-  ## Skip all sub-directories, all files in flat folder.
-  files <- files[which(sapply(files, function(x) file.info(x)$isdir) != TRUE)]
-  
-  ## Very simply checks (to extend later)
-  if (length(files) == 0)
-    warning(paste("MCUPGMA build: no files in directory: ",
-                  copy_path,
-                  " (build failed)"))
+    ## All files in folder
+    files <- list.files(path = copy_path, full.names = TRUE)
 
-  ## Any empty files?
-  if (!all(sapply(files, function(x) file.info(x)$size > 0)))
-    warning(paste("MCUPGMA build: empty files in directory: ",
-                  copy_path,
-                  " (build failed)"))
+    ## Skip all sub-directories, all files in flat folder.
+    files <- files[which(sapply(files, function(x) file.info(x)$isdir) != TRUE)]
 
-  print(paste("INSTALL FROM:", copy_path, "TO:", dest_mcupgma))
+    ## Very simply checks (to extend later)
+    if (length(files) == 0)
+        warning(paste("MCUPGMA build: no files in directory: ",
+                      copy_path,
+                      " (build failed)"))
 
-  ## Write all files. (return is logical for each element)
-  retCopy <- file.copy(files, dest_mcupgma, overwrite = TRUE)
+    ## Any empty files?
+    if (!all(sapply(files, function(x) file.info(x)$size > 0)))
+        warning(paste("MCUPGMA build: empty files in directory: ",
+                      copy_path,
+                      " (build failed)"))
 
-  if (!all(retCopy)) {
-    broken <- files[which(retCopy == FALSE)]
-    print("FILES UNABLE TO COPY:")
-    print(broken)
-    stop(nbErrorMsg("Errors during mcupgma file copies (see list above)"))
-  }
+    print(paste("INSTALL FROM:", copy_path, "TO:", dest_mcupgma))
+
+    ## Write all files. (return is logical for each element)
+    retCopy <- file.copy(files, dest_mcupgma, overwrite = TRUE)
+
+    if (!all(retCopy)) {
+        broken <- files[which(retCopy == FALSE)]
+        print("FILES UNABLE TO COPY:")
+        print(broken)
+        stop(nbErrorMsg("Errors during mcupgma file copies (see list above)"))
+    }
 }
 
-# @TODO Unused?
-# files2 <- Sys.glob(paste0(dest_mcupgma, "/*"))
+## @TODO Unused?
+## files2 <- Sys.glob(paste0(dest_mcupgma, "/*"))
 files <- list.files(path = dest_mcupgma, full.names = TRUE)
 
-# This fix should be in Makevars, but as Gnu make syntax only allowed on
-# Windows, it hastingly added here...
-# @TODO Remove
-# sapply(Sys.glob(paste0("*", SHLIB_EXT)), function(file) {
-
-# Extension is .so, so mask "." as character.
+## This fix should be in Makevars, but as Gnu make syntax only allowed on
+## Windows, it hastingly added here...
+## @TODO Remove
+## Extension is .so, so mask "." as character.
 sapply(list.files(".", pattern = paste(".*\\", SHLIB_EXT, sep=""), full.names = TRUE),
-        function(file) {
-            if (file != tolower(file)) {
-            print(paste("Create:", tolower(file), "from", file))
-            file.copy(c(file), c(tolower(file)), overwrite = TRUE)
-        }
-    })
+       function(file) {
+           if (file != tolower(file)) {
+               print(paste("Create:", tolower(file), "from", file))
+               file.copy(c(file), c(tolower(file)), overwrite = TRUE)
+           }
+       })
 
 ## Copy own (non-mcugpma) libraries (basically only netboost.so) to dest.
 ## Basically the code from R manual
+## files <- Sys.glob(paste0("*", SHLIB_EXT))
 dir.create(dest_lib, recursive = TRUE, showWarnings = FALSE)
-# files <- Sys.glob(paste0("*", SHLIB_EXT))
-files <- list.files(".", pattern = paste("*", SHLIB_EXT, sep=""), full.names = TRUE)
+files <- list.files(".", pattern = paste(".*\\", SHLIB_EXT, sep=""), full.names = TRUE)
 file.copy(files, dest_lib, overwrite = TRUE)
 print(files)
 
-if(file.exists("symbols.rds"))
-  file.copy("symbols.rds", dest_lib, overwrite = TRUE)
+if (file.exists("symbols.rds"))
+    file.copy("symbols.rds", dest_lib, overwrite = TRUE)
